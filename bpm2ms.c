5ea0af8034007d31b70161e51d0198f996777801
CLacaile <clementlacaile@gmail.com>
2019-04-15 17:03:36 +0200

    initial commit

---
+++ bpm2ms.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+#define MS_LEN 20       // length of the ms values
+#define NB_DIV 6        // number of div to display + 1
+
+void printRow(char * cell1, char * cell2) {
+    printf("%s\t|\t%s\n", cell1, cell2);
+}
+
+int main(int argc, char ** args) {
+    double bpm = atof(args[1]);
+    double ms[NB_DIV];
+    char mschar[MS_LEN][NB_DIV];
+    char divchar[MS_LEN][NB_DIV];
+
+    // First row
+    sprintf(divchar[0], "1/1");
+    ms[0] = (bpm/60)*1000;
+    sprintf(mschar[0], "%.0f", ms[0]);
+    printRow("1/1", mschar[0]);
+
+    // Other rows
+    for(int i=1;i<NB_DIV;i++) {
+        sprintf(divchar[i], "1/%.0f", pow(2, i));
+        ms[i] = (ms[i-1]/2);
+        sprintf(mschar[i], "%.0f ms", ms[i]);
+        printRow(divchar[i], mschar[i]);
+    }
+
+    return 0;    
+}
---
+++ readme.md
@@ -0,0 +1,19 @@
+# BPM2MS
+
+Convertisseur de bpm vers ms qui affiche une liste de ms en fonction de la division du tempo saisi.
+Ex:
+
+''' ./bpm2ms 120 
+    TEMPO: 120
+    DIV |   MS
+    1/1 |   2000
+    1/2 |   1000
+    1/4 |   500
+    1/8 |   250
+    1/16|   125 ''' 
+
+Pour lancer un calcul, il faut entrer la commande:
+
+''' ./bpm2ms tempo '''
+
+
